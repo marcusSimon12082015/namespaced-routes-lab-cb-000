6f7e28a1e2c86c36919baf453eb7e431d79677e9
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-12-26 20:12:25 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,8 @@ class SongsController < ApplicationController
   end
 
   def new
-    if Preference.allow_create_songs
+    p = Preference.first
+    if p.allow_create_songs
       @song = Song.new
     else
       redirect_to songs_path
